5c170fe05a5db5fe38c4540ecc46bfae97be3772
Ali H. Caliskan <ali.h.caliskan@gmail.com>
2015-12-01 14:10:20 +0100

    replace tabs with spaces

--- makesha.sh
+++ makesha.sh
@@ -25,7 +25,7 @@ AsRoot() {
 
 GetRcs() {
     if [ -n $rcsrepo ]; then
-    	git clone $rcsrepo $rcs_tempdir
+        git clone $rcsrepo $rcs_tempdir
     else
         print_red "please set recipe repository in /etc/pan.conf"; exit 1
     fi
@@ -34,18 +34,18 @@ GetRcs() {
 AsRoot; GetRcs
 
 if [ -f $pkg_repodir/shasums.tar.xz ]; then
-	rm -rf $pkg_repodir/shasums.tar.xz
+    rm -rf $pkg_repodir/shasums.tar.xz
 fi
 
 for _pkg in $(ls $rcs_tempdir); do
-	if [ -f $rcs_tempdir/$_pkg/recipe ]; then
-		. $rcs_tempdir/$_pkg/recipe
-	fi
-	if  [ -f $pkg_repodir/$pkg-$ver-$rel.$ext ]; then
-		shasum=$(echo "$(sha256sum $pkg_repodir/$pkg-$ver-$rel.$ext | cut -d' ' -f1) ")
-		print_green "$pkg-$ver-$rel: $shasum"
-		echo "sha=$shasum" > $sha_tempdir/$pkg-$ver-$rel
-	fi
+    if [ -f $rcs_tempdir/$_pkg/recipe ]; then
+        . $rcs_tempdir/$_pkg/recipe
+    fi
+    if  [ -f $pkg_repodir/$pkg-$ver-$rel.$ext ]; then
+        shasum=$(echo "$(sha256sum $pkg_repodir/$pkg-$ver-$rel.$ext | cut -d' ' -f1) ")
+        print_green "$pkg-$ver-$rel: $shasum"
+        echo "sha=$shasum" > $sha_tempdir/$pkg-$ver-$rel
+    fi
 done
 
 (cd $sha_tempdir; tar -cpJf $pkg_repodir/shasums.tar.xz ./)
